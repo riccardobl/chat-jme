f8f07b3655b01eee56e9b83f312b341f68f0c2c9
Riccardo Balbo <riccardo0blb@gmail.com>
2023-02-05 12:13:33 +0100

    update docker entry point

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -11,7 +11,15 @@ echo "
     \"JME_HUB_KNOWLEDGE_CUTOFF\":\"$JME_HUB_KNOWLEDGE_CUTOFF\",
     \"JME_HUB_SEARCH_FILTER\":\"$JME_HUB_SEARCH_FILTER\"
     \"DEVICE\":\"$DEVICE\",
-    \"USE_SUMY\":$USE_SUMY
+    \"USE_SUMY\":$USE_SUMY,
+    \"SMART_CACHE\":[
+        [30,70],
+        [50,50],
+        [300,30],
+        [400,20],
+        [0,17]
+    ],
+    \"OPENAI_MODEL\":\"$OPENAI_MODEL\"
 }
 " > "$CONFIG_PATH"
 
